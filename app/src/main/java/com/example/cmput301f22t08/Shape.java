d4a8bcb6d57b6c17c46626c10bbfc02fa695dc3e
Joshua Ji <joshua.ji@jiwhiz.com>
2022-09-28 18:17:43 -0600

    resolve conflict

--- app/src/main/java/com/example/cmput301f22t08/Shape.java
+++ app/src/main/java/com/example/cmput301f22t08/Shape.java
@@ -3,8 +3,6 @@ package com.example.cmput301f22t08;
 public abstract class Shape {
     int x;
     int y;
-    String color = "red";
-
     String color = "yellow";
 
     public Shape(int x, int y) {
