3c324d8d720de75925afe7ffb3ceea8815ce45c2
Clare Chen <77589760+claremaodou@users.noreply.github.com>
2022-10-03 13:50:43 -0600

    Update Shape.java

--- app/src/main/java/com/example/cmput301f22t08/Shape.java
+++ app/src/main/java/com/example/cmput301f22t08/Shape.java
@@ -3,7 +3,7 @@ package com.example.cmput301f22t08;
 public abstract class Shape {
     int x;
     int y;
-    String color = "blue";
+    String color = "yellow";
 
     public Shape(int x, int y) {
         this.x = x;
